73572d017da9651f0a8e638beff99d02f0fa6494
Luccas <lccs.mrm@gmail.com>
2023-04-03 20:04:10 -0300

    Correções.

--- src/main/java/br/com/ada/locasp/demo/controller/PredioController.java
+++ src/main/java/br/com/ada/locasp/demo/controller/PredioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("apartamento")
+@RequestMapping("predio")
 @RestController
 public class PredioController {
 
